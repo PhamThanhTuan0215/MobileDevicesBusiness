26c3322b188404c7430ab3fa2ea2cd0b239b4af0
thanhITSW <nguyenmtthanh13@gmail.com>
2025-01-08 08:31:37 +0700

    add method router manager

--- routers/Manager.js
+++ routers/Manager.js
@@ -3,4 +3,18 @@ const Router = express.Router()
 
 const Controller = require('../controllers/Manager')
 
+Router.post('/', Controller.addNewManager)
+
+Router.get('/', Controller.getAllManager)
+
+Router.get('/:id', Controller.getManagerById)
+
+Router.patch('/:id', Controller.upload , Controller.updateManagerById)
+
+Router.delete('/:id', Controller.deleteManagerByID)
+
+Router.post('/login', Controller.upload , Controller.login)
+
+Router.patch('/changePassword/:id', Controller.upload , Controller.changePassword)
+
 module.exports = Router
